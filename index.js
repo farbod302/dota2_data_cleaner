e731b937cc16a679994c27e9effd5e8f7244ffa0
farbod302 <f.aliakbari302@gmail.com>
2023-12-13 16:30:44 +0330

    sdf

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 const express = require("express")
 const app = express()
 const routs=require("./routs/index")
+const cors=require("cors")
 
-const fs=require("fs")
-const player_games = require("./funcs/players/player_games")
 const keys=Object.keys(routs)
+
+app.use(cors())
 
 keys.forEach(key=>{
     app.use("/"+key,routs[key])
