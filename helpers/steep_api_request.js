8b0e1f7fa4b58c7849db0b2b3592b3c3e2f8fc3d
farbod302 <f.aliakbari302@gmail.com>
2023-12-30 10:09:49 +0330

    add multi api key

--- helpers/steep_api_request.js
+++ helpers/steep_api_request.js
@@ -1,10 +1,15 @@
 
 const { default: axios } = require("axios")
-const { gzip, ungzip } = require('node-gzip');
-const fs = require("fs")
+
+
+const api_keys = [
+    "CBFA72D2C15715642CFA13D130ED91AA",
+    "6FCF589DF7920B997C79A36C38029271"
+]
 
 const steam_request = async (path, payload) => {
-    const params = new URLSearchParams({ ...payload, key: "6FCF589DF7920B997C79A36C38029271" }).toString()
+    const get_random_key = Math.floor(Math.random() * api_keys.length)
+    const params = new URLSearchParams({ ...payload, key: api_keys[get_random_key] }).toString()
     console.log(`https://api.steampowered.com/IDOTA2Match_570/${path}/v1/?${params}`);
     const { data } = await axios.get(`https://api.steampowered.com/IDOTA2Match_570/${path}/v1/?${params}`)
     return data
